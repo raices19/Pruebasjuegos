e3c6bb6967f2b34f4adb76f8ba7b29e4af9dda5b
Carina amairani Díaz Ramírez <1717110615@utectulancingo.edu.mx>
2019-03-16 22:42:34 -0600

    Agregando boton al menu principal del juego

--- app/src/main/java/com/example/memoramavalores/MainActivity.java
+++ app/src/main/java/com/example/memoramavalores/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.memoramavalores;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bttnregreso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        bttnregreso=(Button) findViewById(R.id.btnjuego);
+
+        bttnregreso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pagina2 = new Intent(MainActivity.this,MenuJuego.class);
+                startActivity(pagina2);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/memoramavalores/MenuJuego.java
+++ app/src/main/java/com/example/memoramavalores/MenuJuego.java
@@ -1,13 +1,30 @@
 package com.example.memoramavalores;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuJuego extends AppCompatActivity {
+
+    Button bttnjuego;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_juego);
+
+        bttnjuego=(Button) findViewById(R.id.btnjuego);
+
+        bttnjuego.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pagina2 = new Intent(MenuJuego.this,MainActivity.class);
+                startActivity(pagina2);
+            }
+        });
+
     }
 }
