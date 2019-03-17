355ae2d4b0a33706286510272a57620f357054eb
Carina amairani Díaz Ramírez <1717110615@utectulancingo.edu.mx>
2019-03-16 22:23:40 -0600

    Agregando Menu para entrar al juego y a los niveles del juego

---
+++ app/src/main/java/com/example/memoramavalores/MenuJuego.java
@@ -0,0 +1,13 @@
+package com.example.memoramavalores;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuJuego extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_juego);
+    }
+}
